0cd9f1eca0598ade5213072b1a49f9ca1826d308
Davidson Rafael Correa <rafacaracois@gmail.com>
2020-05-13 23:35:39 -0300

    Modifying style for more lean css file. Tests with Jinja2 loops and layout extends

--- python_app/controlefinanceiro.py
+++ python_app/controlefinanceiro.py
@@ -4,5 +4,16 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/planejamento_geral')
-def titulo(pagina="Planjemento . Visão Geral"):
-    return render_template('template.html', titulo=pagina)
+def planejamento_geral():
+    tcabecalhos = ["Grupo","Tipo","Descrição","Valor Planejado","Valor Consumado"]
+    tdados = []
+    dados = ['Salário Mensal','Receita Efetiva','Salário LFDA','$3400','3400']
+    tdados.append(dados)
+    dados = ['Imóvel','Despesa Efetiva','Pagamento Aluguel','$870','870']
+    tdados.append(dados)
+    return render_template('planejamento_geral.html', tcabecalhos=tcabecalhos, tdados=tdados)
+
+@app.route('/testes')
+def testes():
+    testes = ["Te amo","Te amo","Te amo"]
+    return render_template('teste.html', testes=testes)
