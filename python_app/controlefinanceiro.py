b8c95ab477a17627bffb4fb977bcb5c440c066ce
Davidson Rafael Correa <rafacaracois@gmail.com>
2020-05-19 21:08:16 -0300

    Adding a few of changes. Added venv path for backup

--- python_app/controlefinanceiro.py
+++ python_app/controlefinanceiro.py
@@ -1,17 +1,27 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
 
-@app.route('/planejamento_geral')
+@app.route('/planejamento_geral', methods=["POST", "GET"])
 def planejamento_geral():
     tcabecalhos = ["Grupo","Tipo","Descrição","Valor Planejado","Valor Consumado"]
     tdados = []
-    dados = ['Salário Mensal','Receita Efetiva','Salário LFDA','$3400','3400']
+    req = request.form
+    grupo = req.get("grupo")
+    tipo = req.get("tipo")
+    descricao = req.get("descricao")
+    valorplanejado = req.get("valorplanejado")
+    valorconsumado = req.get("valorconsumado")
+    dados = [grupo,tipo,descricao,valorplanejado,valorconsumado]
     tdados.append(dados)
-    dados = ['Imóvel','Despesa Efetiva','Pagamento Aluguel','$870','870']
-    tdados.append(dados)
+    #dados = ['Imóvel','Despesa Efetiva','Pagamento Aluguel','$870','870']
+    #tdados.append(dados)
     return render_template('planejamento_geral.html', tcabecalhos=tcabecalhos, tdados=tdados)
+
+@app.route('/planejamento_geral/novo')
+def plang_novo():
+    return render_template('plang_novo.html')
 
 @app.route('/testes')
 def testes():
